d742d7900f1d3332fc495eec0a1f4a527745b569
TetianaKolt <106824735+TetianaKolt@users.noreply.github.com>
2023-01-21 15:10:33 +0200

    updated values in testGetPercentCount()

--- src/test/java/hardTests/HardTestClass.java
+++ src/test/java/hardTests/HardTestClass.java
@@ -34,9 +34,9 @@ public class HardTestClass {
     
     @Test
     public void testGetPercentCount(){
-        double number = 100;
+        double number = 200;
         double percent = 15;
-        double expectedResult = 15;
+        double expectedResult = 30;
         double actualResult = calc.getPercentCount(number,percent);
         Assert.assertEquals(actualResult, expectedResult, "When calculating "+percent+" percent of "+ number+", the result is "+expectedResult);
     }
